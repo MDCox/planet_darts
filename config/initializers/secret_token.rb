2e90dac65a6b3ec3896700082cd018bd5c361a3e
Matt <mattcoxpdx@gmail.com>
2014-04-24 09:35:56 -0700

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PlanetDarts::Application.config.secret_key_base = 'c65766060916eb99b98c445a7c0db1fe9d1ebfeaf817670a996ff3ce3f294623838ea2c407e5d240c1b22275a6a39b136bf0cdac12d3a034ea83aa98739a50db'
