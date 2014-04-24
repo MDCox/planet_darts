b5b1da1fc1bfafe2d9a0c5f2ec114e175aa1fc87
Matt <mattcoxpdx@gmail.com>
2014-04-24 10:36:11 -0700

    add avg_score to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :scores
+
+  def avg_score
+    score_total = 0
+    num_of_scores = scores.length
+
+    scores.each do |score|
+      score_total += score
+    end
+
+    return (score_total / num_of_scores)
+  end
 end
