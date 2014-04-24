11469e0dcd730415305f85f8fdd06fa3074aea30
Ben Birdsall & Tara Nelson <mattcoxpdx@gmail.com>
2014-04-24 10:54:12 -0700

    paper styling for interesting colors

--- app/assets/javascripts/gmap_script.js
+++ app/assets/javascripts/gmap_script.js
@@ -1,7 +1,139 @@
 function initialize() {
+  var paper_format = [
+    {
+        "featureType": "administrative",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "stylers": [
+            {
+                "color": "#5f94ff"
+            },
+            {
+                "lightness": 26
+            },
+            {
+                "gamma": 5.86
+            }
+        ]
+    },
+    {},
+    {
+        "featureType": "road.highway",
+        "stylers": [
+            {
+                "weight": 0.6
+            },
+            {
+                "saturation": -85
+            },
+            {
+                "lightness": 61
+            }
+        ]
+    },
+    {
+        "featureType": "road"
+    },
+    {},
+    {
+        "featureType": "landscape",
+        "stylers": [
+            {
+                "hue": "#0066ff"
+            },
+            {
+                "saturation": 74
+            },
+            {
+                "lightness": 100
+            }
+        ]
+    }
+]
+
   var mapOptions = {
     center: new google.maps.LatLng(-34.397, 150.644),
-    zoom: 8
+    zoom: 8,
+    styles: paper_format
   };
   var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 }
