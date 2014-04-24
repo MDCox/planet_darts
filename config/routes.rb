6060ed63bd6b5c632b1ee603ce3b9318cc24dffa
Alex Wood <student@epicodus.com>
2014-04-24 11:13:34 -0700

    create users/new.html.erb and form partial

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ PlanetDarts::Application.routes.draw do
 
   root "pages#index"
   resources :pages, only: [:index]
-  resources :users
+  resources :users, only: [:new, :create]
   resources :sessions
 end
