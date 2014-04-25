1eaa86a6873f5a18296a6cb7532369a1d4884655
vincent rotter & Matt Cox <mattcoxpdx@gmail.com>
2014-04-25 12:10:43 -0700

    not technically modal, but commits map with polyline from position to guess position that appears upon guess submission.

--- app/assets/javascripts/gmap_script.js
+++ app/assets/javascripts/gmap_script.js
@@ -1,5 +1,6 @@
 var marker = null;
 var beginning = new google.maps.LatLng( gon.start_lat, gon.start_long );
+var path;
 
 function initialize() {
   var paper_format = [
@@ -155,10 +156,44 @@ function initialize() {
   };
   var panorama = new google.maps.StreetViewPanorama(document.getElementById("pano"), panoramaOptions);
 
+  var guessMapOptions = {
+    zoom: 2,
+    center: centerPosition,
+    streetViewControl: false
+  };
+
+  var gMap = new google.maps.Map(document.getElementById('guessMap'), guessMapOptions);
+
+  var guessLine = new google.maps.Polyline({
+    geodesic: false,
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.5,
+    strokeWeight: 2
+  });
+
+  guessLine.setMap(gMap);
+
   google.maps.event.addListener(map, 'click', function(e) {
     marker = placeMarker(e.latLng, map, marker);
-
+    path = guessLine.getPath();
+    path.clear();
+    path.push(beginning);
+    path.push(marker.position);
   });
+}
+
+
+function zoomToObject(obj){
+    var bounds = new google.maps.LatLngBounds();
+    var points = obj.getPath().getArray();
+    for (var n = 0; n < points.length ; n++){
+        bounds.extend(points[n]);
+    }
+    gMap.fitBounds(bounds);
+    setTimeout(function() {
+                google.maps.event.trigger(gMap,'resize');
+                gMap.fitBounds(bounds);
+                }, 200);
 }
 
 function placeMarker(position, map, marker) {
@@ -179,10 +214,12 @@ function placeMarker(position, map, marker) {
 function submitGuess() {
   if (marker === null) {
     alert("Guess first, dum-dum!");
+    return false
   } else {
     var $form = $('#new_score')
     $('#score_coord1').val(marker.position);
     $('#score_coord2').val(beginning);
+    return true
   }
 };
 
@@ -190,7 +227,23 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
 $(document).ready(function() {
   $('#new_score').submit(function(event) {
-    submitGuess();
-    return true;
+    if(submitGuess() == true){
+
+      var url = $(this).attr('action');
+
+      $.ajax({
+        type: 'POST',
+        url: url,
+        data: $('#new_score').serialize(),
+        success: function (data) {
+
+        }
+      });
+
+      return false;
+
+    } else {
+      return false;
+    }
   });
 });
--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -3,6 +3,10 @@ class ScoresController < ApplicationController
     points = calc_points(score_params[:coord1], score_params[:coord2])
     score = Score.new(points: points)
     current_user.scores << score
+    respond_to do |f|
+      f.js
+      f.html { redirect_to root_path}
+    end
   end
 
 private
